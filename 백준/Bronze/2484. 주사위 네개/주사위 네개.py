579a4343489df62f7660f4f408cb2fc11d044567
hyeokil <95341353+hyeokil@users.noreply.github.com>
2023-11-30 22:24:20 +0900

    [Bronze II] Title: 주사위 네개, Time: 80 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2484. 주사위 네개/주사위 네개.py
+++ 백준/Bronze/2484. 주사위 네개/주사위 네개.py
@@ -1,22 +1,24 @@
-res_li = []
-for _ in range(int(input())):
-    lst = list(map(int, input().split()))
-    st = len(set(lst))
-    if st == 1:
-        res_li.append(50000 + lst[0] * 5000)
-    elif st == 2:
-        count = lst.count(max(lst))
-        if count == 1:
-            res_li.append(min(lst) * 1000 + 10000)
-        elif count == 3:
-            res_li.append(max(lst) * 1000 + 10000)
-        elif count == 2:
-            res_li.append(max(lst) * 500 + min(lst) * 500 + 2000)
-    elif st == 3:
-        for p in lst:
-            if lst.count(p) == 2:
-                res_li.append(p * 100 + 1000)
-                break
-    else:
-        res_li.append(max(lst) * 100)
-print(max(res_li))
+def Main():
+    ans = []
+    for _ in range(int(input())):
+        lst = list(map(int, input().split()))
+        st = len(set(lst))
+        if st == 1:
+            ans.append(50000 + lst[0] * 5000)
+        elif st == 2:
+            count = lst.count(max(lst))
+            if count == 1:
+                ans.append(min(lst) * 1000 + 10000)
+            elif count == 3:
+                ans.append(max(lst) * 1000 + 10000)
+            elif count == 2:
+                ans.append(max(lst) * 500 + min(lst) * 500 + 2000)
+        elif st == 3:
+            for p in lst:
+                if lst.count(p) == 2:
+                    ans.append(p * 100 + 1000)
+                    break
+        else:
+            ans.append(max(lst) * 100)
+    print(max(ans))
+Main()

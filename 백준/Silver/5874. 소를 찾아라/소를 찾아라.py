f93e13151f1e864497a983813d5a6d608ecdb650
hyeokil <95341353+hyeokil@users.noreply.github.com>
2023-12-07 13:11:34 +0900

    [Silver III] Title: 소를 찾아라, Time: 56 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/5874. 소를 찾아라/소를 찾아라.py
+++ 백준/Silver/5874. 소를 찾아라/소를 찾아라.py
@@ -1,18 +1,11 @@
 def Main():
     green = input()
-    back = []
-    front = []
+    back = 0
     ans = 0
     for i in range(1,len(green)):
         if green[i-1:i+1] == '((':
-            back.append(i)
+            back+= 1
         elif green[i-1:i+1] == '))':
-            front.append(i)
-    fl = len(front)
-    for i in back:
-        for j in range(fl) :
-            if i < front[j] :
-                ans += fl - j
-                break
+            ans += back
     print(ans)
 Main()

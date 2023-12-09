6277350098ef9b16600220aafe56fc4b914dee35
hyeokil <95341353+hyeokil@users.noreply.github.com>
2023-12-09 19:18:18 +0900

    [Gold III] Title: 수열과 쿼리 15, Time: 804 ms, Memory: 42732 KB -BaekjoonHub

--- 백준/Gold/14427. 수열과 쿼리 15/수열과 쿼리 15.py
+++ 백준/Gold/14427. 수열과 쿼리 15/수열과 쿼리 15.py
@@ -4,34 +4,32 @@ sys.setrecursionlimit(10**7)
 
 def init(node, start, end):
     if start == end :
-        tree[node] = [lst[start],start]
+        tree[node] = start
         return tree[node]
     mid = (start+end)//2
-    tempA = init(2*node, start, mid)
-    tempB = init(2*node+1,mid+1,end)
-    if tempA[0] == tempB[0]:
-        tree[node] = [tempA[0],min(tempA[1],tempB[1])]
-    elif tempA[0] < tempB[0] :
-        tree[node] = tempA
+    idxA = init(2*node, start, mid)
+    idxB = init(2*node+1,mid+1,end)
+    if lst[idxA] == lst[idxB]:
+        tree[node] = min(idxA,idxB)
+    elif lst[idxA] < lst[idxB] :
+        tree[node] = idxA
     else:
-        tree[node] = tempB
+        tree[node] = idxB
     return tree[node]
 
-def update(node,start,end,idx,val):
+def update(node,start,end,idx):
     if start > idx or end < idx :
         return
-    if start == end:
-        tree[node] = [val,idx]
-        return
-    mid = (start+end)//2
-    update(2*node,start,mid,idx,val)
-    update(2*node+1,mid+1,end,idx,val)
-    if tree[2*node][0] == tree[2*node+1][0]:
-        tree[node] = [tree[2*node][0],min(tree[2*node][1],tree[2*node+1][1])]
-    elif tree[2*node][0] < tree[2*node+1][0] :
-        tree[node] = tree[2*node]
-    else:
-        tree[node] = tree[2*node+1]
+    if start != end:
+        mid = (start+end)//2
+        update(2*node,start,mid,idx)
+        update(2*node+1,mid+1,end,idx)
+        if lst[tree[2*node]] == lst[tree[2*node+1]]:
+            tree[node] = min(tree[2*node],tree[2*node+1])
+        elif lst[tree[2*node]] < lst[tree[2*node+1]] :
+            tree[node] = tree[2*node]
+        else:
+            tree[node] = tree[2*node+1]
 
 
 N = int(input())
@@ -41,6 +39,7 @@ init(1,0,N-1)
 for m in range(int(input())):
     query = list(map(int,input().split()))
     if query[0] == 1:
-        update(1,0,N-1,query[1]-1,query[2])
+        lst[query[1]-1] = query[2]
+        update(1,0,N-1,query[1]-1)
     else:
-        print(tree[1][1]+1)
+        print(tree[1]+1)

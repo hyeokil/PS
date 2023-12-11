8cb875284313d40b39944fe8918f985aea10fd54
hyeokil <95341353+hyeokil@users.noreply.github.com>
2023-12-11 21:46:01 +0900

    [Platinum IV] Title: 수열과 쿼리 22, Time: 600 ms, Memory: 57948 KB -BaekjoonHub

--- 백준/Platinum/16978. 수열과 쿼리 22/수열과 쿼리 22.py
+++ 백준/Platinum/16978. 수열과 쿼리 22/수열과 쿼리 22.py
@@ -1,54 +1,55 @@
 import sys
 input = sys.stdin.readline
 
+def Main():
+    def Modify(i, diff):
+        while i <= N:
+            tree[i] += diff
+            i += (i & -i)
+    def Sum(i):
+        result = 0
+        while i>0:
+            result+=tree[i]
+            i -= (i & -i)
+        return result
 
-def Modify(i, diff):
-    while i <= N:
-        tree[i] += diff
-        i += (i & -i)
-def Sum(i):
-    result = 0
-    while i>0:
-        result+=tree[i]
-        i -= (i & -i)
-    return result
-
-N = int(input())
-arr= [0]+list(map(int, input().split()))
-tree = [0]*(N+1)
-q1 = [0]
-q2 = []
-ans = []
-M = int(input())
-idx = 0
-for i in range(1,N+1):
-    Modify(i,arr[i])
-for m in range(M):
-    lst = list(map(int, input().split()))
-    if lst[0] == 1:
-        q1.append((lst[1],lst[2]))
-    else:
-        q2.append((lst[1],lst[2],lst[3],idx))
-        idx += 1
-        ans.append(0)
-q2.sort(key=lambda e: e[0],reverse=True)
-while q2 :
-    cnt, A, B, ix = q2.pop()
-    if cnt == 0:
-        ans[ix] = Sum(B)-Sum(A-1)
-    else:
-        q2.append((cnt,A,B,ix))
-        break
-for i in range(1,len(q1)):
-    a,b = q1[i]
-    Modify(a,b-arr[a])
-    arr[a] = b
-    while q2:
+    N = int(input())
+    arr= [0]+list(map(int, input().split()))
+    tree = [0]*(N+1)
+    q1 = [0]
+    q2 = []
+    ans = []
+    M = int(input())
+    idx = 0
+    for i in range(1,N+1):
+        Modify(i,arr[i])
+    for m in range(M):
+        lst = list(map(int, input().split()))
+        if lst[0] == 1:
+            q1.append((lst[1],lst[2]))
+        else:
+            q2.append((lst[1],lst[2],lst[3],idx))
+            idx += 1
+            ans.append(0)
+    q2.sort(key=lambda e: e[0],reverse=True)
+    while q2 :
         cnt, A, B, ix = q2.pop()
-        if cnt == i:
-            ans[ix] = Sum(B) - Sum(A - 1)
+        if cnt == 0:
+            ans[ix] = Sum(B)-Sum(A-1)
         else:
             q2.append((cnt,A,B,ix))
             break
-for i in ans:
-    print(i)
+    for i in range(1,len(q1)):
+        a,b = q1[i]
+        Modify(a,b-arr[a])
+        arr[a] = b
+        while q2:
+            cnt, A, B, ix = q2.pop()
+            if cnt == i:
+                ans[ix] = Sum(B) - Sum(A - 1)
+            else:
+                q2.append((cnt,A,B,ix))
+                break
+    for i in ans:
+        print(i)
+Main()

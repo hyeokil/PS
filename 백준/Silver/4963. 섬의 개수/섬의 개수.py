ce1f8454363ce88d360265b9862b840ed36a69c7
hyeokil <95341353+hyeokil@users.noreply.github.com>
2023-12-12 16:07:15 +0900

    [Silver II] Title: 섬의 개수, Time: 72 ms, Memory: 34072 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.py
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.py
@@ -7,20 +7,19 @@ def Main():
         if W == 0 and H == 0 :
             break
         arr = [list(map(int,input().split())) for _ in range(H)]
-        visited = [[False]*W for _ in range(H)]
         ans = 0
         for i in range(H):
             for j in range(W):
-                if not visited[i][j] and arr[i][j] == 1 :
-                    visited[i][j] = True
+                if arr[i][j] == 1 :
                     q = deque()
                     q.append((i,j))
+                    arr[i][j] = 0
                     while q :
                         x,y = q.popleft()
                         for dx,dy in [(1,0),(-1,0),(0,1),(0,-1),(1,1),(1,-1),(-1,1),(-1,-1)]:
                             nx,ny = dx+x,dy+y
-                            if 0<=nx<H and 0<=ny<W and arr[nx][ny] == 1 and not visited[nx][ny]:
-                                visited[nx][ny] =True
+                            if 0<=nx<H and 0<=ny<W and arr[nx][ny] == 1:
+                                arr[nx][ny] = 0
                                 q.append((nx,ny))
                     ans += 1
         print(ans)

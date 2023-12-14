0f46eb7a7bda4b9b5539baac49249dc6f812ff1d
hyeokil <95341353+hyeokil@users.noreply.github.com>
2023-12-14 20:41:59 +0900

    [Silver III] Title: 팰린드롬 만들기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.py
@@ -0,0 +1,24 @@
+def Main():
+    S = input()
+    setS = list(set(S))
+    setS.sort()
+    cnt = []
+    isPal = 0
+    res = ''
+    ans = ''
+    for i in setS:
+        cnt.append(S.count(i))
+    for i in range(len(cnt)):
+        if cnt[i]%2 == 1:
+            isPal+=1
+            res += setS[i]
+    if isPal > 1 :
+        print("I'm Sorry Hansoo")
+    else:
+        for i in range(len(cnt)):
+            for j in range(cnt[i]//2):
+                ans+=setS[i]
+        ansR = reversed(ans)
+        ans += res +''.join([*ansR])
+        print(ans)
+Main()

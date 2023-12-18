8156ac860dbcc76ca0ab1db580141b13add77e62
hyeokil <95341353+hyeokil@users.noreply.github.com>
2023-12-18 14:14:04 +0900

    [Gold V] Title: 회문은 회문아니야!!, Time: 44 ms, Memory: 32100 KB -BaekjoonHub

--- 백준/Gold/15927. 회문은 회문아니야！！/회문은 회문아니야！！.py
+++ 백준/Gold/15927. 회문은 회문아니야！！/회문은 회문아니야！！.py
@@ -1,16 +1,10 @@
 def Main():
     S = input()
-    s,e = 0,len(S)-1
-    while s<e:
-        if S[s] != S[e] :
-            return len(S)
-        s+=1
-        e-=1
-    s,e = 0,len(S)-2
-    while s<e:
-        if S[s] != S[e] :
-            return len(S)-1
-        s+=1
-        e-=1
-    return -1
-print(Main())
+    L = len(S)
+    if S == S[-1]*L:
+        print(-1)
+    elif S == S[::-1]:
+        print(L-1)
+    else:
+        print(L)
+Main()

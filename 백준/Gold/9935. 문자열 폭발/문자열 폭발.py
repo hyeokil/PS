b243cdec69a30ced42c439c67eafc68d4cf3286a
hyeokil <95341353+hyeokil@users.noreply.github.com>
2023-12-27 15:04:06 +0900

    [Gold IV] Title: 문자열 폭발, Time: 192 ms, Memory: 42300 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
@@ -2,10 +2,11 @@ def Main():
     S = input()
     bomb = list(input())
     bomb_size=len(bomb)
+    last_b = bomb[-1]
     st = []
     for s in S:
         st.append(s)
-        if st[-bomb_size:] == bomb :
+        if st[-1] == last_b and st[-bomb_size:] == bomb :
             del st[-bomb_size:]
     if st :
         print(''.join(st))

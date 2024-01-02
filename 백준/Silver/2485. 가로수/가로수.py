1d87bdd8c622a0897efe177ca4b65a33279a28ed
hyeokil <95341353+hyeokil@users.noreply.github.com>
2024-01-02 16:36:40 +0900

    [Silver IV] Title: 가로수, Time: 92 ms, Memory: 37096 KB -BaekjoonHub

--- 백준/Silver/2485. 가로수/가로수.py
+++ 백준/Silver/2485. 가로수/가로수.py
@@ -1,21 +1,18 @@
-import sys
+import sys,math
 input = sys.stdin.readline
-
-def gcd(a,b):
-    while b > 0 :
-        a,b = b,a%b
-    return a
-N = int(input())
-lst = []
-tmp = int(input())
-for i in range(N-1):
-    n = int(input())
-    lst.append(n-tmp)
-    tmp = n
-res = lst[0]
-for i in range(1,N-1):
-    res = gcd(res,lst[i])
-ans = 0
-for i in range(N-1):
-    ans += lst[i]//res-1
-print(ans)
+def Main():
+    N = int(input())
+    lst = []
+    tmp = int(input())
+    for i in range(N-1):
+        n = int(input())
+        lst.append(n-tmp)
+        tmp = n
+    res = lst[0]
+    for i in range(1,N-1):
+        res = math.gcd(res,lst[i])
+    ans = 0
+    for i in range(N-1):
+        ans += lst[i]//res-1
+    print(ans)
+Main()

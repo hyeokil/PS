e8b5f5ff68a402c3d9cd4cd5e6d82f7dfcbe04fa
hyeokil <95341353+hyeokil@users.noreply.github.com>
2024-01-10 21:29:54 +0900

    [level unrated] Title: n의 배수, Time: 0.01 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181937. n의 배수/n의 배수.java
+++ 프로그래머스/unrated/181937. n의 배수/n의 배수.java
@@ -1,11 +1,7 @@
 class Solution {
     public int solution(int num, int n) {
-        int answer = 0;
-        if (num%n==0) {
-            answer = 1;
-        }else {
-            answer = 0;
-        }
+        int answer = num%n==0 ?  1 : 0;
+        
         return answer;
     }
 }

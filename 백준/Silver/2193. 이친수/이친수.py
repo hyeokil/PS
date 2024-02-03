166dbd96c61749e748d1969b95e4f0bc8b8f5314
hyeokil <95341353+hyeokil@users.noreply.github.com>
2024-02-03 21:11:33 +0900

    [Silver III] Title: 이친수, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2193. 이친수/이친수.py
+++ 백준/Silver/2193. 이친수/이친수.py
@@ -1,10 +1,8 @@
-def Main():
-    N = int(input())
-    dp0 = 0
-    dp1 = 1
-    for i in range(2,N+1):
-        tmp = dp0+dp1
-        dp0=dp1
-        dp1 = tmp
-    print(dp1)
-Main()
+N = int(input())
+dp0 = 0
+dp1 = 1
+for i in range(2,N+1):
+    tmp = dp0+dp1
+    dp0=dp1
+    dp1 = tmp
+print(dp1)

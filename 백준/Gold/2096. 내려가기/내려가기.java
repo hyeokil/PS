60cce13986c3cebd137d9f000f7f449386eb3f54
hyeokil <95341353+hyeokil@users.noreply.github.com>
2024-02-04 17:47:57 +0900

    [Gold V] Title: 내려가기, Time: 316 ms, Memory: 39952 KB -BaekjoonHub

---
+++ 백준/Gold/2096. 내려가기/내려가기.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+import static java.lang.Integer.max;
+import static java.lang.Integer.min;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        int maxA=0,maxB=0,maxC=0,minA=0,minB=0,minC=0;
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken()),
+                    b=Integer.parseInt(st.nextToken()),
+                    c=Integer.parseInt(st.nextToken());
+            int A = a + max(maxA, maxB), B = b + max(max(maxA, maxB), maxC), C = c + max(maxB, maxC);
+            int D = a + min(minA, minB), E = b + min(min(minA, minB), minC), F = c + min(minB, minC);
+            maxA = A;maxB = B;maxC = C;minA = D;minB = E;minC = F;
+
+        }
+        bw.write(Integer.toString(max(max(maxA,maxB),maxC))+" "+min(min(minA,minB),minC));
+        bw.close();
+    }
+}

0cd3bfda463c1e465da9faee13b56fd2d989821c
hyeokil <95341353+hyeokil@users.noreply.github.com>
2024-02-18 00:08:10 +0900

    [Silver V] Title: 올림픽, Time: 92 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -7,9 +7,13 @@ def Main():
         arr.append((b,c,d,a))
     arr.sort(reverse=True)
     idx = 0
+    res = 0
     for i in range(1,N+1):
         if arr[i][:3] != arr[i-1][:3]:
             idx+=1
+            idx+=res
+            res = 0
+        else:res+=1
         if arr[i][3] == M:
             print(idx)
             break

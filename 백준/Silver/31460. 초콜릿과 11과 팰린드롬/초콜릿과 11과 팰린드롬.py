1a02307207ff3c593c073c22a4e97c8eb5f7cb25
hyeokil <95341353+hyeokil@users.noreply.github.com>
2024-03-01 20:47:17 +0900

    [Silver IV] Title: 초콜릿과 11과 팰린드롬, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/31460. 초콜릿과 11과 팰린드롬/초콜릿과 11과 팰린드롬.py
+++ 백준/Silver/31460. 초콜릿과 11과 팰린드롬/초콜릿과 11과 팰린드롬.py
@@ -1,11 +1,4 @@
-def Main():
-    for tc in range(int(input())):
-        N = int(input())
-        ans = ''
-        if N>1:
-            for i in range(N):
-                if i==0 or i==N-1:ans+='1'
-                else:ans+='2'
-            print(ans)
-        else:print(0)
-Main()
+for tc in range(int(input())):
+    N = int(input())
+    if N>1:print("1"+"2"*(N-2)+"1")
+    else:print(0)

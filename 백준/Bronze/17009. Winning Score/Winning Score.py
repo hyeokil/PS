f76c87ee1ced5cbe6ce555acfdced9ea287660ee
hyeokil <95341353+hyeokil@users.noreply.github.com>
2024-03-11 23:20:13 +0900

    [Bronze IV] Title: Winning Score, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/17009. Winning Score/Winning Score.py
@@ -0,0 +1,14 @@
+a=int(input())
+b=int(input())
+c=int(input())
+d=int(input())
+e=int(input())
+f=int(input())
+A = a*3+b*2+c
+B = d*3+e*2+f
+if A>B:
+    print("A")
+elif A<B:
+    print("B")
+else:
+    print("T")

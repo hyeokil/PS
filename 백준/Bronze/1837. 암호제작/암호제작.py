2129b374de1b87c5ae555e50c1c82bb2e824fe4e
hyeokil <95341353+hyeokil@users.noreply.github.com>
2024-03-24 14:52:21 +0900

    [Bronze III] Title: 암호제작, Time: 260 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1837. 암호제작/암호제작.py
@@ -0,0 +1,12 @@
+N,K = map(int,input().split())
+flag = False
+ans = 0
+for i in range(2,K):
+    if N%i==0:
+        flag=True
+        ans = i
+        break
+if flag :print("BAD",ans)
+else:print("GOOD")
+
+

87cd12e1042be9b368f641db23f3b6f726e28b22
hyeokil <95341353+hyeokil@users.noreply.github.com>
2024-04-01 22:35:50 +0900

    [Gold III] Title: 파일 합치기, Time: 508 ms, Memory: 40560 KB -BaekjoonHub

--- 백준/Gold/11066. 파일 합치기/파일 합치기.py
+++ 백준/Gold/11066. 파일 합치기/파일 합치기.py
@@ -1,21 +1,37 @@
 import sys
 input = sys.stdin.readline
 
-def Main():
-    for i in range(int(input())):
-        n = int(input())
-        f = list(map(int, input().split()))
-        d = [[0] * (n + 1) for i in range(n + 1)]
-    
-        for i in range(n - 1):
-            d[i][i + 1] = f[i] + f[i + 1]
-            for j in range(i + 2, n):
-                d[i][j] = d[i][j - 1] + f[j]
-    
-        for v in range(2, n):
-            for i in range(n - v):
-                j = i + v
-                d[i][j] += min([d[i][k] + d[k + 1][j] for k in range(i, j)])
-    
-        print(d[0][n - 1])
-Main()
+
+def solution():
+    K = int(input())
+    chapters = tuple(map(int, input().split()))
+    sums = [0]
+    for i in range(K):
+        sums.append(sums[-1] + chapters[i])
+    dp = [[0] * (K + 1) for _ in range(K + 1)]
+    m_dp = [[0] * (K + 1) for _ in range(K + 1)]
+    for i in range(K + 1):
+        m_dp[i][i] = i
+
+    for dist in range(1, K):
+        for l in range(K - dist):
+            r = l + dist
+            min_price = 50_000_000
+            min_price_mid = l
+            for m in range(m_dp[l][r - 1], m_dp[l + 1][r] + 1):
+                price = dp[l][m] + dp[m + 1][r] + sums[r + 1] - sums[l]
+                if price < min_price:
+                    min_price = price
+                    min_price_mid = m
+            dp[l][r] = min_price
+            m_dp[l][r] = min_price_mid
+    print(dp[0][K - 1])
+
+
+def main():
+    T = int(input())
+    for _ in range(T):
+        solution()
+
+
+main()

79b0b2200183d82c96a1f8f050233f45af903846
hyeokil <95341353+hyeokil@users.noreply.github.com>
2024-04-02 22:32:26 +0900

    [Gold V] Title: Steps, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/4395. Steps/Steps.py
+++ 백준/Gold/4395. Steps/Steps.py
@@ -2,15 +2,8 @@ import sys
 input = sys.stdin.readline
 
 def Main():
-    for tc in range(int(input())):
-        A,B = map(int,input().split())
-        C = B-A
-        n = int(((-1)+(1+4*C)**(1/2))/2)
-        tmp = C - n * (n + 1)
-        ans = 2 * n
-        if 0 < tmp <= n + 1:
-            ans += 1
-        elif tmp > n + 1:
-            ans += 2
-        print(ans)
-Main()
+    T = int(input())
+    for _ in range(T):
+        x, y = map(int, input().split())
+        print(int(2 * (y - x) ** 0.5 - 1e-9))
+Main() 

9ac4cc52f0196c74ad99049d9e3e64d90444cd27
hyeokil <95341353+hyeokil@users.noreply.github.com>
2024-04-07 19:04:31 +0900

    [Gold V] Title: 선 긋기, Time: 3844 ms, Memory: 235692 KB -BaekjoonHub

--- 백준/Gold/2170. 선 긋기/선 긋기.py
+++ 백준/Gold/2170. 선 긋기/선 긋기.py
@@ -1,17 +1,19 @@
 import sys
 input = sys.stdin.readline
 
-N = int(input())
-L = [list(map(int, input().split())) for _ in range(N)]
-L.sort()
-mx = L[0][1]
-ans = L[0][1]-L[0][0]
-for i in range(1,N):
-    if L[i][1] <= mx:continue
-    elif L[i][0]<mx:
-        ans+=L[i][1]-mx
-        mx = L[i][1]
-    else:
-        ans += L[i][1]-L[i][0]
-        mx = L[i][1]
-print(ans)
+def Main():
+    N = int(input())
+    L = [list(map(int, input().split())) for _ in range(N)]
+    L.sort()
+    mx = L[0][1]
+    ans = L[0][1]-L[0][0]
+    for i in range(1,N):
+        if L[i][1] <= mx:continue
+        elif L[i][0]<mx:
+            ans+=L[i][1]-mx
+            mx = L[i][1]
+        else:
+            ans += L[i][1]-L[i][0]
+            mx = L[i][1]
+    print(ans)
+Main()

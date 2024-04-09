f4be91f02290c1c314074dc7cf6fa04b20d6f651
hyeokil <95341353+hyeokil@users.noreply.github.com>
2024-04-09 22:15:58 +0900

    [Silver III] Title: 배열 복원하기, Time: 160 ms, Memory: 37772 KB -BaekjoonHub

--- 백준/Silver/16967. 배열 복원하기/배열 복원하기.py
+++ 백준/Silver/16967. 배열 복원하기/배열 복원하기.py
@@ -1,16 +1,18 @@
 import sys
 input = sys.stdin.readline
 
-H,W,X,Y=map(int,input().split())
-arr = [list(map(int,input().split())) for i in range(H+X)]
-for i in range(H):
-    for j in range(W):
-        if i>=X and j>=Y:
-            arr[i][j] -= arr[i-X][j-Y]
-            print(arr[i][j],end="")
-        else:
-            print(arr[i][j],end="")
-        if j==W-1:
-            print(sep="\n")
-        else:
-            print(end=" ")
+def Main():
+    H,W,X,Y=map(int,input().split())
+    arr = [list(map(int,input().split())) for i in range(H+X)]
+    for i in range(H):
+        for j in range(W):
+            if i>=X and j>=Y:
+                arr[i][j] -= arr[i-X][j-Y]
+                print(arr[i][j],end="")
+            else:
+                print(arr[i][j],end="")
+            if j==W-1:
+                print(sep="\n")
+            else:
+                print(end=" ")
+Main()

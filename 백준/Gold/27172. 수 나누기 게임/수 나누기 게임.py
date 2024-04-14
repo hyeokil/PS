0d54af9823414d5b042f2a7c2727d5007e61ea23
hyeokil <95341353+hyeokil@users.noreply.github.com>
2024-04-14 17:21:24 +0900

    [Gold V] Title: 수 나누기 게임, Time: 2760 ms, Memory: 124936 KB -BaekjoonHub

--- 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.py
+++ 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.py
@@ -1,29 +1,21 @@
 import sys
 input = sys.stdin.readline
 
-def f(a):
-    cnt = 0
-    if a==1:
-        return 0
-    for j in range(1,int(a**0.5)+1):
-        if a%j==0 :
-            if check[j][0]:
-                check[j][1]+=1
-                cnt+=1
-            if j!=1 and j*j !=a and check[a//j][0]:
-                check[a//j][1]+=1
-                cnt+=1
-    return cnt
-
-N = int(input())
-arr = list(map(int, input().split()))
-check = [[False,0] for _ in range(max(arr)+1)]
-for i in arr:
-    check[i][0]=True
-for i in range(N):
-    check[arr[i]][1]-=f(arr[i])
-for i in range(N):
-    if i==N-1:
-        print(check[arr[i]][1],end="")
-    else:
-        print(check[arr[i]][1],end=" ")
+def Main():
+    N = int(input())
+    arr = list(map(int, input().split()))
+    mx= max(arr)+1
+    check = [[False,0] for _ in range(mx)]
+    for i in arr:
+        check[i][0]=True
+    for a in arr:
+        for i in range(a*2,mx,a):
+            if check[i][0]:
+                check[a][1]+=1
+                check[i][1]-=1
+    for i in range(N):
+        if i==N-1:
+            print(check[arr[i]][1],end="")
+        else:
+            print(check[arr[i]][1],end=" ")
+Main()

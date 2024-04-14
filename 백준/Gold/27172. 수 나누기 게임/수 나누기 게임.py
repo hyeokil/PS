098f080e3469d1d09f493af9ac7a39a3f7d8cc1c
hyeokil <95341353+hyeokil@users.noreply.github.com>
2024-04-14 17:25:09 +0900

    [Gold V] Title: 수 나누기 게임, Time: 712 ms, Memory: 52788 KB -BaekjoonHub

--- 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.py
+++ 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.py
@@ -1,21 +1,23 @@
 import sys
-input = sys.stdin.readline
+
 
 def Main():
+    input = sys.stdin.readline
     N = int(input())
-    arr = list(map(int, input().split()))
+    *arr, = map(int, input().split())
     mx= max(arr)+1
-    check = [[False,0] for _ in range(mx)]
+    check = [False]*mx
+    res = [0]*mx
     for i in arr:
-        check[i][0]=True
+        check[i]=True
     for a in arr:
         for i in range(a*2,mx,a):
-            if check[i][0]:
-                check[a][1]+=1
-                check[i][1]-=1
+            if check[i]:
+                res[a]+=1
+                res[i]-=1
     for i in range(N):
         if i==N-1:
-            print(check[arr[i]][1],end="")
+            print(res[arr[i]],end="")
         else:
-            print(check[arr[i]][1],end=" ")
+            print(res[arr[i]],end=" ")
 Main()

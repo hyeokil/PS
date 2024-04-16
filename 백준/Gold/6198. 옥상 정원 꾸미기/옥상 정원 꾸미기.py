642e9c106f0e68c010ac0a4b9a30d73c4deaff63
hyeokil <95341353+hyeokil@users.noreply.github.com>
2024-04-16 23:51:21 +0900

    [Gold V] Title: 옥상 정원 꾸미기, Time: 80 ms, Memory: 34556 KB -BaekjoonHub

--- 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.py
+++ 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.py
@@ -1,13 +1,13 @@
 import sys
 input = sys.stdin.readline
 
-buildings = [int(input()) for _ in range(int(input()))]
-st = []
-res = 0
-
-for building in buildings:
-  while st and st[-1]<=building:
-    st.pop()
-  st.append(building)
-  res += len(st)-1
-print(res)
+def Main():
+    buildings = [int(input()) for _ in range(int(input()))]
+    st = []
+    ans = 0
+    for building in buildings:
+      while st and st[-1]<=building:st.pop()
+      st.append(building)
+      ans += len(st)-1
+    print(ans)
+Main()

9bfa4f00392abfdcc02c81a7ef04a5fafea3da78
hyeokil <95341353+hyeokil@users.noreply.github.com>
2024-04-25 21:51:13 +0900

    [Gold V] Title: 동전, Time: 60 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/9084. 동전/동전.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+def Main():
+    T = int(input())
+    for _ in range(T):
+        N = int(input())
+        coins = list(map(int, input().split()))
+        M = int(input())
+        dp = [0]*(M+1)
+        dp[0]=1
+        for coin in coins:
+            for i in range(coin,M+1):
+                if dp[i-coin] > 0:
+                    dp[i]+=dp[i-coin]
+        print(dp[M])
+
+Main()

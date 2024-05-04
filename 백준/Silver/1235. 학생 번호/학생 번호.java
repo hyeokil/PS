0864cc05fcb6f499021afac4729f973957beb9b2
hyeokil <95341353+hyeokil@users.noreply.github.com>
2024-05-04 16:43:00 +0900

    [Silver IV] Title: 학생 번호, Time: 120 ms, Memory: 16036 KB -BaekjoonHub

---
+++ 백준/Silver/1235. 학생 번호/학생 번호.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        List<String> list = new ArrayList<>();
+        while (N-- > 0) {
+            list.add(br.readLine());
+        }
+        int L = list.get(0).length();
+        int ans = 1;
+        while (true) {
+            HashSet<String> set = new HashSet<>();
+            for (int i = 0; i < list.size(); i++) {
+                set.add(list.get(i).substring(L-ans,L));
+            }
+            if (set.size() == list.size()) {
+                break;
+            } else {
+                ans += 1;
+            }
+        }
+        System.out.println(ans);
+
+    }
+}

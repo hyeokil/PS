efefe97d332f727f2bc35cc80bebefdc87a4b40f
hyeokil <95341353+hyeokil@users.noreply.github.com>
2024-05-05 22:49:00 +0900

    [Bronze IV] Title: 파티가 끝나고 난 뒤, Time: 84 ms, Memory: 11700 KB -BaekjoonHub

---
+++ 백준/Bronze/2845. 파티가 끝나고 난 뒤/파티가 끝나고 난 뒤.java
@@ -0,0 +1,27 @@
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        for (int tc = 0; tc < 5; tc++) {
+            int a = Integer.parseInt(st.nextToken());
+            System.out.printf((a-N*M)+" ");
+
+        }
+    }
+}

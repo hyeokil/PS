dd618aa88e4dd233b9c745ee79b7500eb3931e03
hyeokil <95341353+hyeokil@users.noreply.github.com>
2024-05-09 19:38:56 +0900

    [Silver V] Title: 임시 반장 정하기, Time: 340 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1268. 임시 반장 정하기/임시 반장 정하기.py
+++ 백준/Silver/1268. 임시 반장 정하기/임시 반장 정하기.py
@@ -1,20 +1,22 @@
 import sys
 input = sys.stdin.readline
 
-N = int(input())
-arr = [list(map(int, input().split())) for _ in range(N)]
-mx = 0
-ans =1
-for i in range(N):
-    visited = [False]*N
-    visited[i] = True
-    v = 0
-    for j in range(5):
-        for k in range(N):
-            if not visited[k] and arr[i][j] == arr[k][j]:
-                visited[k]=True
-                v+=1
-    if v>mx :
-        mx = v
-        ans = i+1
-print(ans)
+def Main():
+    N = int(input())
+    arr = [list(map(int, input().split())) for _ in range(N)]
+    mx = 0
+    ans =1
+    for i in range(N):
+        visited = [False]*N
+        visited[i] = True
+        v = 0
+        for j in range(5):
+            for k in range(N):
+                if not visited[k] and arr[i][j] == arr[k][j]:
+                    visited[k]=True
+                    v+=1
+        if v>mx :
+            mx = v
+            ans = i+1
+    print(ans)
+Main()

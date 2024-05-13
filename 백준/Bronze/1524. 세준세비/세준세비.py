f198b9bfc824b355ddc51163bf06829d4b368bdd
hyeokil <95341353+hyeokil@users.noreply.github.com>
2024-05-13 20:14:19 +0900

    [Bronze I] Title: 세준세비, Time: 144 ms, Memory: 41552 KB -BaekjoonHub

--- 백준/Bronze/1524. 세준세비/세준세비.py
+++ 백준/Bronze/1524. 세준세비/세준세비.py
@@ -4,14 +4,7 @@ for _ in range(T):
     N,M = map(int,input().split())
     lstN = list(map(int,input().split()))
     lstM = list(map(int, input().split()))
-    lstN.sort(reverse=True)
-    lstM.sort(reverse=True)
-    while len(lstN)>0 and len(lstM)>0:
-        if lstN[-1] >= lstM[-1]:
-            lstM.pop()
-        else:
-            lstN.pop()
-    if len(lstN)>0:
+    if max(lstN)>=max(lstM):
         print("S")
     else:
         print("B")

4b39e6095111c3a22d329f69c710b834ebfbe893
hyeokil <95341353+hyeokil@users.noreply.github.com>
2024-05-17 22:51:24 +0900

    [Gold V] Title: 배열 탈출, Time: 7964 ms, Memory: 264368 KB -BaekjoonHub

---
+++ 백준/Gold/11909. 배열 탈출/배열 탈출.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+arr = [list(map(int,input().split())) for _ in range(N)]
+dp = [[float('inf')]*N for _ in range(N)]
+dp[0][0]=0
+for i in range(N):
+    for j in range(N):
+        if 0<=i-1:
+            if arr[i-1][j] > arr[i][j]:
+                dp[i][j] = min(dp[i-1][j],dp[i][j])
+            else:
+                dp[i][j] = min(dp[i-1][j]+arr[i][j]-arr[i-1][j]+1,dp[i][j])
+        if 0<=j-1:
+            if arr[i][j-1] > arr[i][j]:
+                dp[i][j] = min(dp[i][j-1],dp[i][j])
+            else:
+                dp[i][j] = min(dp[i][j-1]+arr[i][j]-arr[i][j-1]+1,dp[i][j])
+print(dp[N-1][N-1])

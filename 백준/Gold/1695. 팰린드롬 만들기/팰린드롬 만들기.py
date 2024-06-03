732ad06e06ee3d331e405e3132e6cd41c40c45fd
hyeokil <95341353+hyeokil@users.noreply.github.com>
2024-06-03 17:08:43 +0900

    [Gold III] Title: 팰린드롬 만들기, Time: 3908 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1695. 팰린드롬 만들기/팰린드롬 만들기.py
+++ 백준/Gold/1695. 팰린드롬 만들기/팰린드롬 만들기.py
@@ -1,13 +1,16 @@
 import sys
 input = sys.stdin.readline
-N = int(input())
-lst = list(map(int, input().split()))
-dp = [[0]*N for _ in range(2)]
-for i in range(N-1,-1,-1):
-    for j in range(i+1,N):
-        x = i%2
-        if lst[i] != lst[j]:
-            dp[x][j] = min(dp[x][j-1],dp[x-1][j])+1
-        else:
-            dp[x][j] = dp[x-1][j-1]
-print(dp[0][N-1])
+
+def Main():
+    N = int(input())
+    lst = list(map(int, input().split()))
+    dp = [[0]*N for _ in range(2)]
+    for i in range(N-1,-1,-1):
+        for j in range(i+1,N):
+            x = i%2
+            if lst[i] != lst[j]:
+                dp[x][j] = min(dp[x][j-1],dp[x-1][j])+1
+            else:
+                dp[x][j] = dp[x-1][j-1]
+    print(dp[0][N-1])
+Main()    

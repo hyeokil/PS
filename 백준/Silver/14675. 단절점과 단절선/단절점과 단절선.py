c4cf3df230989074348da3bc50cf3a9f5629f7a9
hyeokil <95341353+hyeokil@users.noreply.github.com>
2024-06-05 16:51:39 +0900

    [Silver I] Title: 단절점과 단절선, Time: 196 ms, Memory: 31824 KB -BaekjoonHub

--- 백준/Silver/14675. 단절점과 단절선/단절점과 단절선.py
+++ 백준/Silver/14675. 단절점과 단절선/단절점과 단절선.py
@@ -1,19 +1,21 @@
 import sys
 input = sys.stdin.readline
 
-N = int(input())
-node = [0]*(N+1)
-for i in range(N-1):
-    a,b = map(int,input().split())
-    node[a]+=1
-    node[b] += 1
-M = int(input())
-for i in range(M):
-    t,k = map(int,input().split())
-    if t==1:
-        if node[k]>1:
-            print("yes")
+def Main():
+    N = int(input())
+    node = [0]*(N+1)
+    for i in range(N-1):
+        a,b = map(int,input().split())
+        node[a]+=1
+        node[b] += 1
+    M = int(input())
+    for i in range(M):
+        t,k = map(int,input().split())
+        if t==1:
+            if node[k]>1:
+                print("yes")
+            else:
+                print("no")
         else:
-            print("no")
-    else:
-        print("yes")
+            print("yes")
+Main()

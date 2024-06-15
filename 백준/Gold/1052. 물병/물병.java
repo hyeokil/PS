f18a5fea48b8357fa7e5a1d2cd15526296543dda
hyeokil <95341353+hyeokil@users.noreply.github.com>
2024-06-15 20:31:42 +0900

    [Gold V] Title: 물병, Time: 76 ms, Memory: 11568 KB -BaekjoonHub

---
+++ 백준/Gold/1052. 물병/물병.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int ans = 0;
+        while (Integer.bitCount(N) > K) {
+            ans += 1;
+            N += 1;
+        }
+        System.out.println(ans);
+    }
+}
+

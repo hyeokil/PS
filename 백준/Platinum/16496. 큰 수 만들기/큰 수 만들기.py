cae0c9e65fc5d88fae76e32905e3562057dff589
hyeokil <95341353+hyeokil@users.noreply.github.com>
2024-06-24 18:36:00 +0900

    [Platinum V] Title: 큰 수 만들기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Platinum/16496. 큰 수 만들기/큰 수 만들기.py
+++ 백준/Platinum/16496. 큰 수 만들기/큰 수 만들기.py
@@ -1,10 +1,9 @@
-from functools import cmp_to_key
 import sys
 input = sys.stdin.readline
 
 def Main():
     N = int(input())
     lst = input().split()
-    lst.sort(key=cmp_to_key(lambda x,y:-1 if int(x+y)>int(y+x) else 1))
-    print("".join(lst) if sum(map(int,lst)) else 0 )
+    lst.sort(key=lambda x:x*9,reverse=True)
+    print(int("".join(lst)))
 Main()

0b8f2c6b59ea634373233e23094d2db10f892678
hyeokil <95341353+hyeokil@users.noreply.github.com>
2024-07-01 19:39:41 +0900

    [Bronze I] Title: 2진수 8진수, Time: 1860 ms, Memory: 94992 KB -BaekjoonHub

---
+++ 백준/Bronze/1373. 2진수 8진수/2진수 8진수.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String S = br.readLine();
+        BigInteger N = new BigInteger(S, 2);
+        String ans = N.toString(8);
+        System.out.println(ans);
+
+    }
+}
+

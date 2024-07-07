c1839b47739722c5a90c1bafa92e666ef1b89459
hyeokil <95341353+hyeokil@users.noreply.github.com>
2024-07-07 19:19:18 +0900

    [Silver II] Title: 생태학, Time: 740 ms, Memory: 91836 KB -BaekjoonHub

---
+++ 백준/Silver/4358. 생태학/생태학.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String S;
+        int cnt = 0;
+        HashMap<String, Integer> map = new HashMap<>();
+        while ((S = br.readLine()) != null && !S.isEmpty()) {
+            if (map.containsKey(S)) {
+                map.put(S, map.get(S) + 1);
+            }else {
+                map.put(S, 1);
+            }
+            cnt++;
+        }
+        List<String> keys = new ArrayList<>(map.keySet());
+        Collections.sort(keys);
+        for (String key : keys) {
+            System.out.println(key+" "+String.format("%.4f",(float)map.get(key)/cnt*100));
+        }
+    }
+}
+

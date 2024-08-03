f69508133d05b424e9350440097101ade11c2731
hyeokil <95341353+hyeokil@users.noreply.github.com>
2024-08-03 10:45:59 +0900

    [Gold V] Title: 휴먼 파이프라인, Time: 508 ms, Memory: 55708 KB -BaekjoonHub

--- 백준/Gold/22981. 휴먼 파이프라인/휴먼 파이프라인.java
+++ 백준/Gold/22981. 휴먼 파이프라인/휴먼 파이프라인.java
@@ -1,23 +1,25 @@
 import java.io.*;
 import java.util.*;
 
+
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
-        long K = Long.parseLong(st.nextToken());
-        long ans = Long.MAX_VALUE, tmp = 0L;
+        long K = Long.parseLong(st.nextToken()), res = 0L,tmp=0L,ans;
         List<Long> list = new ArrayList<Long>();
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
+        for (int i=0; i<N; i++) {
             list.add(Long.parseLong(st.nextToken()));
         }
         Collections.sort(list);
-        for (int i = 1; i < N; i++) {
-            tmp = (list.get(0) * i) + (list.get(i) * (N - i));
-            ans = Math.min(ans, K % tmp == 0 ? K / tmp : K / tmp + 1);
+        for (int i=1; i<N; i++) {
+            tmp=(list.get(0)*i)+(list.get(i)*(N-i));
+            res = Math.max(res, tmp);
         }
+        ans = K%res==0?K/res:K/res+1;
         System.out.println(ans);
+
     }
 }

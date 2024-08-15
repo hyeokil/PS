7b65ff357cbaee2572e788ea3aa62d09a2c13520
hyeokil <95341353+hyeokil@users.noreply.github.com>
2024-08-15 23:22:55 +0900

    [Silver III] Title: Equalising Audio, Time: 452 ms, Memory: 40400 KB -BaekjoonHub

--- 백준/Silver/25996. Equalising Audio/Equalising Audio.java
+++ 백준/Silver/25996. Equalising Audio/Equalising Audio.java
@@ -1,56 +1,36 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st = null;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder ans = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken()),X = Integer.parseInt(st.nextToken());
+        long[] amplitude = new long[N];
+        long sum = 0;
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            amplitude[i] = Long.parseLong(st.nextToken());
+            sum += amplitude[i]*amplitude[i];
+        }
+        
+        if (X == 0) {
+            for (int i = 0; i < N; i++) {
+                ans.append(0).append(" ");
+            }
+        } else {
+            double k = Math.sqrt((double) X*N / sum);
 
-		st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());	// 진폭의 개수
-		int x = Integer.parseInt(st.nextToken());	// 목표 값
+            for (int i = 0; i < N; i++) {
+                if (amplitude[i] == 0) {
+                    ans.append(0).append(" ");
+                } else {
+                    ans.append(amplitude[i] * k).append(" ");
+                }
+            }
+        }
+        System.out.println(ans);
 
-		long[] amplitude = new long[n];	// 진폭
-		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < n; i++) {
-			amplitude[i] = Long.parseLong(st.nextToken());
-		}
-
-		// 평균 인지 음량
-		long currentLoudness = 0;
-		for (int i = 0; i < n; i++) {
-			currentLoudness += amplitude[i] * amplitude[i];
-		}
-		double avg = (double) currentLoudness / n;
-
-		if (x == 0) {
-			for (int i = 0; i < n; i++) {
-				sb.append(0).append(" ");
-			}
-		} else {
-			// 정규화 상수 k 계산
-			double k = Math.sqrt((double) x / avg);
-	
-			// 진폭 재정규화 및 출력
-			for (int i = 0; i < n; i++) {
-				if (amplitude[i] == 0) {
-					sb.append(0).append(" ");
-				} else {
-					sb.append(amplitude[i] * k).append(" ");
-				}
-			}
-		}
-
-		// 출력
-		br.close();
-		bw.write(sb.toString());
-		bw.flush();
-		bw.close();
-	}
+    }
 }

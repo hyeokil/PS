dae1c3f511fbf10c0e64ce1c76d86ac022c70fd3
hyeokil <95341353+hyeokil@users.noreply.github.com>
2024-08-25 15:35:56 +0900

    [Silver III] Title: 피자 (Large), Time: 64 ms, Memory: 11492 KB -BaekjoonHub

---
+++ 백준/Silver/14607. 피자 （Large）/피자 （Large）.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long N = Long.parseLong(br.readLine());
+        System.out.println((N*N-N)/2);
+    }
+}

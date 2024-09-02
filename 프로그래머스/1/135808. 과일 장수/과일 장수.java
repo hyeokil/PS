7ab2ad50fb5aa6602c26d9365f0657431320f919
hyeokil <95341353+hyeokil@users.noreply.github.com>
2024-09-02 16:48:04 +0900

    [level 1] Title: 과일 장수, Time: 62.13 ms, Memory: 137 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -4,13 +4,9 @@ class Solution {
     public int solution(int k, int m, int[] score) {
         int answer = 0;
         Arrays.sort(score);
-        int cnt = 0;
-        for (int i = score.length-1 ;i>=0;i--){
-            cnt++;
-            if (cnt == m){
-                answer += score[i]*m;
-                cnt = 0;
-            }
+        for (int i = score.length-m ;i>=0;i-=m){
+            answer += score[i]*m;
+        
         }
         return answer;
     }

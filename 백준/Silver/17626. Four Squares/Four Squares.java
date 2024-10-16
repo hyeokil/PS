7328767220532f85cf4822004196f6f949100803
hyeokil <95341353+hyeokil@users.noreply.github.com>
2024-10-16 23:20:55 +0900

    [Silver III] Title: Four Squares, Time: 100 ms, Memory: 12140 KB -BaekjoonHub

---
+++ 백준/Silver/17626. Four Squares/Four Squares.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] dp = new int[N + 1];
+        dp[1]= 1;
+        for (int i = 2; i <= N; i++) {
+            dp[i] = dp[i - 1];
+            for (int j = 1; j*j<=i; j++) {
+                dp[i] = Math.min(dp[i], dp[i - (j*j)]);
+            }
+            dp[i]++;
+        }
+        System.out.println(dp[N]);
+    }
+}
